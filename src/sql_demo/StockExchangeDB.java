207499f03d118fc4b84aeef678684c827fc1f78c
naglec13 <naglec13@gmail.com>
2020-03-28 01:54:30 +0300

    Initial commit

---
+++ src/sql_demo/StockExchangeDB.java
@@ -0,0 +1,6 @@
+package sql_demo;
+
+public class StockExchangeDB {
+    public static final String DB_URL = "jdbc:h2:D:\\home_Projects\\udemy\\practice-java-building-projects\\DataBase\\JDBC_H2\\db\\StockExchange";
+    public static final String DB_DRIVER = "org.h2.Driver";
+}
