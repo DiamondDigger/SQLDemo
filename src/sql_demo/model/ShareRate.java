733d616cf240460982043610b98a24a4f2dcdcd2
naglec13 <naglec13@gmail.com>
2020-03-29 23:28:57 +0300

    ..ShareRate in package model

---
+++ src/sql_demo/model/ShareRate.java
@@ -0,0 +1,39 @@
+package sql_demo.model;
+
+import java.time.LocalDateTime;
+
+public class ShareRate extends BaseModel {
+    private LocalDateTime operationDate;
+    private long share;
+    private double rate;
+
+    public ShareRate(LocalDateTime operationDate, long share, double rate) {
+        this.operationDate = operationDate;
+        this.share = share;
+        this.rate = rate;
+    }
+
+    public LocalDateTime getOperationDate() {
+        return operationDate;
+    }
+
+    public void setOperationDate(LocalDateTime operationDate) {
+        this.operationDate = operationDate;
+    }
+
+    public long getShare() {
+        return share;
+    }
+
+    public void setShare(long share) {
+        this.share = share;
+    }
+
+    public double getRate() {
+        return rate;
+    }
+
+    public void setRate(double rate) {
+        this.rate = rate;
+    }
+}
