be3ab37c9989f7ec511200c4d41f0b9387461318
naglec13 <naglec13@gmail.com>
2020-03-29 23:09:42 +0300

    ..

--- src/sql_demo/model/BaseModel.java
+++ src/sql_demo/model/BaseModel.java
@@ -1,5 +1,7 @@
 package sql_demo.model;
 
+import java.util.Objects;
+
 public class BaseModel {
     protected long id;
 
@@ -15,5 +17,18 @@ public class BaseModel {
 
     public long getId() {
         return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BaseModel baseModel = (BaseModel) o;
+        return id == baseModel.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
