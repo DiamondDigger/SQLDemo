e9223ddf8ad530fa85daea24ddcdc34af457c6f3
naglec13 <naglec13@gmail.com>
2020-03-30 23:54:12 +0300

    ..BaseTable in repository model

---
+++ src/sql_demo/repository/BaseTable.java
@@ -0,0 +1,11 @@
+package sql_demo.repository;
+
+import java.io.Closeable;
+import java.io.IOException;
+
+public class BaseTable implements Closeable {
+    @Override
+    public void close() throws IOException {
+
+    }
+}
